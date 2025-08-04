622352fc61482327b449685005ea1fb87ed0f2c2
Chinmay Satam <chinmaysatam809@gmail.com>
2025-08-05 02:09:03 +0530

    Studied If Else

---
+++ js/conditional.js
@@ -0,0 +1,17 @@
+// console.log("Hello...");
+
+// let age = 45;
+
+// if(age>18){
+//     console.log("You can drive");
+// }
+
+// else{
+//     console.log("You can't drive");
+// }
+
+let a = 6;
+let b = 14;
+
+let c = a>b ? (a-b) : (b-a);
+console.log(c);
