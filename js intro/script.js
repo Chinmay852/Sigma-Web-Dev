d9f1744ae497691007df32c182541000e3a40d04
Chinmay Satam <chinmaysatam809@gmail.com>
2025-08-04 20:07:45 +0530

    Introduction to JavaScript

---
+++ js intro/script.js
@@ -0,0 +1,31 @@
+console.log("Hello")
+
+var a = 5;
+var b = 6;
+var c = "Chinmay"
+
+console.log(a+b+b+1);
+console.log(typeof a, typeof b, typeof c)
+
+let x = "Chinmay"
+let y = 22;
+let z = 3.14;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+let o = {
+    "name":"chinmay",
+    "job code": 852,
+    "is_handsome":true
+}
+
+console.log(o)
+o.salary = "100 Crores";
+console.log(o)
+o.salary = "500 Crores";
+console.log(o)
