364a6051479a7b9aeb6f48d7d8a9dc52d7c4ed36
Chinmay Satam <chinmaysatam809@gmail.com>
2025-08-07 14:16:15 +0530

    Learn Strings Today

---
+++ js/strings.js
@@ -0,0 +1,23 @@
+console.log("This is String Tutorial")
+let a = "Chinmay";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+console.log(a[7])
+
+console.log(a.length)
+
+let my_name = "Chinmay";
+let friend = "Shivam";
+console.log("Hi my name is " + my_name + " and my friend name is " + friend)
+console.log(`Hi my name is ${my_name} and my friend name is ${friend}`) // Template Literals
+
+
+let b = "Chinmay"
+console.log(b.toUpperCase())
+console.log(b.slice(1,4))
+console.log(b.replace("Ch", "77"))
