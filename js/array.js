aa531c916685bb364e3becdf32992fb243b7cd02
Chinmay Satam <chinmaysatam809@gmail.com>
2025-08-08 17:39:34 +0530

    Array

---
+++ js/array.js
@@ -0,0 +1,15 @@
+// let arr = [1,2,3,4,5,6,7]
+
+// console.log(arr)
+
+
+// let a1 = [1,2,3]
+// let a2 = [4,5,6]
+// let a3 = [7,8,9]
+
+// let a4 = a1.concat (a2,a3)
+
+// console.log(a4)
+
+let b = [7,9,8,11,15,18,2,6]
+console.log(b.sort())
